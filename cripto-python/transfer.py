9cd9953f626eb209913d749ca58cb611269ade6d
Pau Velarde <42307381+vpauv@users.noreply.github.com>
2023-06-15 17:25:46 -0600

    Update transfer.py

--- cripto-python/transfer.py
+++ cripto-python/transfer.py
@@ -4,13 +4,13 @@ def transfer_nft(algod_client,acct1,acct2,created_asset):
     sp = algod_client.suggested_params()
     # Create transfer transaction
     xfer_txn = transaction.AssetTransferTxn(
-        sender=acct1.address,
+        sender=acct1[1],
         sp=sp,
-        receiver=acct2.address,
+        receiver=acct2[1],
         amt=1,
         index=created_asset,
     )
-    signed_xfer_txn = xfer_txn.sign(acct1.private_key)
+    signed_xfer_txn = xfer_txn.sign(acct1[0])
     txid = algod_client.send_transaction(signed_xfer_txn)
     print(f"Sent transfer transaction with txid: {txid}")
 
@@ -34,7 +34,7 @@ def create_contract_trans(algod_client,teal_code,acct1):
     # Crea una transacción de creación de contrato
     params = algod_client.suggested_params()
     txn = transaction.ApplicationCreateTxn(
-        sender=acct1[0],
+        sender=acct1[1],
         sp=params,
         on_complete=transaction.OnComplete.NoOpOC,
         approval_program=teal_code,
@@ -57,5 +57,4 @@ def create_contract_trans(algod_client,teal_code,acct1):
     print("Contrato desplegado con éxito. ID del contrato:", contract_id)
     
     return contract_id
-        
         
